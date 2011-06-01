61a6cbdc046afb0e43b3a3c74edb1be0e0ca3dd1
abloemeke <andreas.bloemeke@sap.com>
2011-06-01 17:59:56 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myfirstrubyrails::Application.config.secret_token = '7b93dc3984e61b1d1475e0b3eef8886950f58b6fe1cf5d830b5a7c5a73af7be5a2ef59bf01d245c1bea04d2c504da46615f3adf734c110ea7716e59fcd4a4a90'
